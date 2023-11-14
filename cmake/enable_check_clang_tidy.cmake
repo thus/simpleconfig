e58a22f9d65c3c10e6125ed11b57e57441d56f11
Mats Klepsland <mats.klepsland@gmail.com>
2023-11-14 14:32:14 +0100

    Change how linters are added to CMake
    Make it possible to turn linters ON/OFF individually. Check that
    linters that are turned ON actually exist.

--- cmake/enable_check_clang_tidy.cmake
+++ cmake/enable_check_clang_tidy.cmake
@@ -1,9 +1,16 @@
 # Makes it possible to run 'make check-clang-tidy'
 macro(enable_check_clang_tidy)
+    find_program(clang_tidy clang-tidy)
+    if (NOT clang_tidy)
+        message(FATAL_ERROR "Could not find linter 'clang-tidy'")
+    endif()
+
     set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
     set(compdb ${CMAKE_BINARY_DIR}/compile_commands.json)
     set(lint_dir ${CMAKE_SOURCE_DIR}/src/*.c)
     set(config_file ${CMAKE_SOURCE_DIR}/.clang-tidy)
-    add_custom_target(check-clang-tidy clang-tidy -p ${compdb} --config-file=${config_file} ${lint_dir} 2>/dev/null)
+
+    add_custom_target(check-clang-tidy ${clang_tidy} -p ${compdb} --config-file=${config_file} ${lint_dir} 2>/dev/null)
     list(APPEND all_linters check-clang-tidy)
 endmacro(enable_check_clang_tidy)#
--- cmake/enable_check_cppcheck.cmake
+++ cmake/enable_check_cppcheck.cmake
@@ -1,5 +1,10 @@
 # Makes it possible to run 'make check-cppcheck'
 macro(enable_check_cppcheck)
-    add_custom_target(check-cppcheck cppcheck --quiet --error-exitcode=1 ${PROJECT_SOURCE_DIR}/src/*.c)
+    find_program(cppcheck cppcheck)
+    if (NOT cppcheck)
+        message(FATAL_ERROR "Could not find linter 'cppcheck'")
+    endif()
+
+    add_custom_target(check-cppcheck ${cppcheck} --quiet --error-exitcode=1 ${PROJECT_SOURCE_DIR}/src/*.c)
     list(APPEND all_linters check-cppcheck)
 endmacro(enable_check_cppcheck)#
