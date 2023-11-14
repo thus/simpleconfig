af7c14490ea71068348981a85afe9db286b7d5c4
Mats Klepsland <mats.klepsland@gmail.com>
2023-11-14 14:32:14 +0100

    Replace ${PROJECT_SOURCE_DIR} with ${sconf_SOURCE_DIR}

--- cmake/enable_check_clang_tidy.cmake
+++ cmake/enable_check_clang_tidy.cmake
@@ -8,8 +8,8 @@ macro(enable_check_clang_tidy)
     set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
     set(compdb ${CMAKE_BINARY_DIR}/compile_commands.json)
-    set(lint_dir ${CMAKE_SOURCE_DIR}/src/*.c)
-    set(config_file ${CMAKE_SOURCE_DIR}/.clang-tidy)
+    set(lint_dir ${sconf_SOURCE_DIR}/src/*.c)
+    set(config_file ${sconf_SOURCE_DIR}/.clang-tidy)
 
     add_custom_target(check-clang-tidy ${clang_tidy} -p ${compdb} --config-file=${config_file} ${lint_dir} 2>/dev/null)
     list(APPEND all_linters check-clang-tidy)
--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,7 +1,7 @@
 # File containing CMake macros
 
-include("${PROJECT_SOURCE_DIR}/cmake/enable_asan.cmake")
-include("${PROJECT_SOURCE_DIR}/cmake/enable_ubsan.cmake")
-include("${PROJECT_SOURCE_DIR}/cmake/enable_coverage.cmake")
-include("${PROJECT_SOURCE_DIR}/cmake/enable_check_clang_tidy.cmake")
-include("${PROJECT_SOURCE_DIR}/cmake/enable_check_cppcheck.cmake")
+include("${sconf_SOURCE_DIR}/cmake/enable_asan.cmake")
+include("${sconf_SOURCE_DIR}/cmake/enable_ubsan.cmake")
+include("${sconf_SOURCE_DIR}/cmake/enable_coverage.cmake")
+include("${sconf_SOURCE_DIR}/cmake/enable_check_clang_tidy.cmake")
+include("${sconf_SOURCE_DIR}/cmake/enable_check_cppcheck.cmake")
