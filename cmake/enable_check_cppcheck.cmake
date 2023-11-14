704f9bfb305b803f1b79735ed6617d95f35a50f0
Mats Klepsland <mats.klepsland@gmail.com>
2023-11-14 14:32:14 +0100

    Add 'make check-cppcheck' command to CMake
    Makes it possible to lint the source files using cppcheck.

---
+++ cmake/enable_check_cppcheck.cmake
@@ -0,0 +1,4 @@
+# Makes it possible to run 'make check-cppcheck'
+macro(enable_check_cppcheck)
+    add_custom_target(check-cppcheck cppcheck --quiet --error-exitcode=1 ${PROJECT_SOURCE_DIR}/src/*.c)
+endmacro(enable_check_cppcheck)#
--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -3,3 +3,4 @@
 include("${PROJECT_SOURCE_DIR}/cmake/enable_asan.cmake")
 include("${PROJECT_SOURCE_DIR}/cmake/enable_coverage.cmake")
 include("${PROJECT_SOURCE_DIR}/cmake/enable_check_clang_tidy.cmake")
+include("${PROJECT_SOURCE_DIR}/cmake/enable_check_cppcheck.cmake")
